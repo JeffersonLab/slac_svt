b4b2ccb0b6b6cddfe93bff66798e3026c7f3116e
Per Hansson Adrian <per.hansson80@gmail.com>
2017-09-11 12:14:20 -0400

    help script for amending author for commits

---
+++ amend_last_commit_author.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#Usage: git commit --amend --author "Per Hansson Adrian <per.hansson80@gmail.com>" --no-edit
+AUTHOR=$1
+echo "New author: ${AUTHOR}"
+#git commit --amend --author $AUTHOR --no-edit
+
+
